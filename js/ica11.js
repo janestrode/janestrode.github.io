d5c8cd9a65f59d3f6c5568cf67fc980266f732f1
janestrode <jast7081@colorado.edu>
2024-11-05 10:43:10 -0700

    ica week 11

---
+++ js/ica11.js
@@ -0,0 +1,8 @@
+
+function tellFortune(numberChildren, partnerName, location, jobTitle){
+    return("you will be a "+jobTitle+" in "+location+ ", and married to "+partnerName+" with "+numberChildren+" kids.")
+}
+
+console.log(tellFortune("2","Harold", "Las Vegas", "relator"))
+console.log(tellFortune("7", "Timothee Chalamet", "Munzie, Indiana", "urologist"))
+console.log(tellFortune("1","your lesbian lover","a DIY tiny house", "cashier at a patagonia outlet store in winter park"))
---
+++ js/wk_11.js
@@ -0,0 +1,22 @@
+// let myArray = [1,2, "three", 4];
+// myArray
+
+//console.log(document.querySelector('button'));
+//console.log(document.querySelectorAll('button'));
+//console.log(document.querySelectorAll('button')[1]);
+//console.log(document.getElementsByClassName('my_button'));
+//console.log(document.getElementsByClassName('my_button')[2]);
+
+hello("jane"); //this calls the function
+
+function hello(name){
+    alert("hello "+ name)
+}
+//remainders, if your number when divided by two has a remainder if 0, itll return true
+function even(n){
+    if(n%2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
