7d30ee6d89bdd5adaa47666552d3eae886683f9c
janestrode <jast7081@colorado.edu>
2024-11-07 16:18:53 -0700

    changing images

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const pics = ["../img/pic5.jpg", "../img/pic4.jpg", "../img/pic3.jpg", "../img/pic2.jpg", "../img/pic1.jpg"]
+const pics = ["../img/dog1.JPG", "../img/dog2.JPG", "../img/dog3.JPG", "../img/dog4.JPG", "../img/dog5.JPG"]
 /* Declaring the alternative text for each image file */
 const alts = ["pic 5","pic 4", "pic 3", "pic 2", "pic 1"]
 
