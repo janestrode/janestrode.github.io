83448d6ba5154f2b86ff2c01804e566f92d17683
janestrode <jast7081@colorado.edu>
2024-11-07 16:29:25 -0700

    trying images again

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const pics = ["../img/dog1.JPG", "../img/dog2.JPG", "../img/dog3.JPG", "../img/dog4.JPG", "../img/dog5.JPG"]
+const pics = ["../img/AD1.jpg", "../img/AD2.webp", "../img/AD3.jpg", "../img/AD4.avif", "../img/AD5.webp"]
 /* Declaring the alternative text for each image file */
-const alts = ["pic 5","pic 4", "pic 3", "pic 2", "pic 1"]
+const alts = ["characters","michael", "gob", "tobias", "lindsay"]
 
 /* Looping through images */
 for (let i=0; i < 5; i++){
