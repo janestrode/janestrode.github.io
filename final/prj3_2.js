d212aaffe549827124f3138a25066f920558431c
janestrode <jast7081@colorado.edu>
2024-12-07 17:38:13 -0700

    more work, adding to json

---
+++ final/prj3_2.js
@@ -0,0 +1,29 @@
+let subdomain = window.location.href.slice(window.location.href.lastIndexOf("/")+1, window.location.href.lastIndexOf("."));
+console.log(subdomain);
+
+fetch(`../final/projects.json`)
+    .then(response =>{
+        return response.json();
+    }).then(projects => {
+        proj = projects;
+        findProjectinJSON(projects);
+        // parseData(projects);
+    }).catch(err =>{
+        console.log(`error ${err}`);
+    })
+
+function findProjectinJSON(projects){
+    for(let i=0; i<projects.projects.length; i++){
+        if(projects.projects[i].subdomain == subdomain){
+            buildPage(projects.projects[i]);
+            break;
+        }else{
+            continue;
+        }
+    }
+}
+
+function buildPage(project){
+    console.log(project);
+    document.getElementById("project").innerHTML += `<h1>${project.name}</h1>`;
+}
